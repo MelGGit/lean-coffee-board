89d322cbfb74ad8000bf356237848382962f61d2
Mel Grosser <grosser.mel@live.de>
2021-05-30 15:30:49 +0200

    final

--- routes/cards.js
+++ routes/cards.js
@@ -1,51 +1,30 @@
 const express = require('express')
-const uuidv4 = require('uuid').v4
 const router = express.Router()
-
-let cards = [
-  {
-    title: 'The way to coffeve',
-    author: 'Mel',
-    votes: 1,
-    id: '0',
-  },
-  {
-    title: 'Coffeve v2',
-    author: 'Donutu Trumpet',
-    votes: 100,
-    id: '1',
-  },
-]
+const Card = require('../models/Cards')
 
 // get all cards
-router.get('/', (req, res, next) => {
-  res.json(cards)
+router.get('/', async (req, res, next) => {
+  res.json(await Card.find())
 })
 
 // get specific card
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   const { id } = req.params
-  const foundCard = cards.find(card => card.id === id)
-  foundCard ? res.json(foundCard) : next()
+  res.json(await Card.findById(id))
 })
 
-router.post('/', (req, res, next) => {
-  const newCard = { ...req.body, id: uuidv4() }
-  cards.push(newCard)
-  res.status(201).json(newCard)
+router.post('/', async (req, res, next) => {
+  res.status(201).json(await Card.create(req.body))
 })
 
-router.patch('/:id', (req, res, next) => {
+router.patch('/:id', async (req, res, next) => {
   const { id } = req.params
-  const cardIndex = cards.findIndex(card => card.id === id)
-  cards[cardIndex] = { ...cards[cardIndex], ...req.body }
-  res.json(cards[cardIndex])
+  res.json(await Card.findByIdAndUpdate(id, req.body))
 })
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
   const { id } = req.params
-  cards = cards.filter(card => card.id !== id)
-  res.sendStatus(204)
+  res.sendStatus(204).json(await Card.findByIdAndDelete(id))
 })
 
 module.exports = router
